cc17f452f00f267cab8c5b6568948c1c14a8274c
mjjstockman <mjjstockman@gmail.com>
2022-05-14 17:47:17 +0000

    Add home test_views and inital tests

---
+++ home/test_views.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+
+# Create your tests here.
+class TestViews(TestCase):
+
+    def test_home_page(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home/index.html')
+
+# if __name__ == '__main__':
+#     unittest.main()
