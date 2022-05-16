eaac57ab035c43faa126fd3258d1b20d502cf949
mjjstockman <mjjstockman@gmail.com>
2022-05-16 14:21:58 +0000

    Add gig, song and setlist to admin

--- setlist/admin.py
+++ setlist/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . models import Gig, Song, Setlist
 
 # Register your models here.
+admin.site.register(Gig)
+admin.site.register(Song)
+admin.site.register(Setlist)
