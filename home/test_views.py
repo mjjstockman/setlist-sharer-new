68b4dd4c7d8de8312ec0c684aa5b6365293841c3
mjjstockman <mjjstockman@gmail.com>
2022-05-16 10:49:08 +0000

    Correct test_home_page template used

--- home/test_views.py
+++ home/test_views.py
@@ -6,7 +6,8 @@ class TestViews(TestCase):
     def test_home_page(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'home/index.html')
+        self.assertTemplateUsed(response, 'index.html')
+        self.assertTemplateUsed(response, 'base.html')
 
 if __name__ == '__main__':
     unittest.main()
