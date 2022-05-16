176b9bcee26498765c7e666a78a0dd5398c402cf
mjjstockman <mjjstockman@gmail.com>
2022-05-16 14:15:40 +0000

    Add initial models to setlist app

--- setlist/models.py
+++ setlist/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+STATUS = ((0, 'Waiting Confirmation'), (1, 'Published'))
 
 # Create your models here.
+class Gig(models.Model):
+    city = models.CharField(max_length=200)
+    # date =
+    # venue = 
+
+    def __str__(self):
+        return self.city
+
+
+class Song(models.Model):
+    title = models.CharField(max_length=200)
+    # release =
+
+    def __str__(self):
+        return self.title
+
+class Release(models.Model):
+    title = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
+
+class Venue(models.Model):
+    name = models.CharField(max_length=200)
+    city = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Setlist(models.Model):
+    # gig = models.OneToOneField(Gig, on_delete=models.CASCADE, null=True)
+    # user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    # song = models.ManyToManyField(Song)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    # def __str__(self):
+    #     return self.gig.city
+
