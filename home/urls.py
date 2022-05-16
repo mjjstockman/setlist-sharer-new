82617fa6f6bda08a045f6058a12f74f7e487d039
mjjstockman <mjjstockman@gmail.com>
2022-05-16 09:54:31 +0000

    Add index template, url and view

---
+++ home/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    # path('create/', views.createSetlist, name='create-setlist'),
+    # # path('create-setlist/', views.createSetlist, name='create-setlist'),
+    # path('', views.setlist, name='setlist'),
+    # path('update/<str:pk>/', views.updateSetlist, name='update-setlist'),
+    # path('delete/<str:pk>/', views.deleteSetlist, name='delete-setlist'),
+]
