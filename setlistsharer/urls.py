7e0326f90f1e4cf17ec0cab14cdd7ae3d699559d
mjjstockman <mjjstockman@gmail.com>
2022-05-17 07:05:58 +0000

    Add CRUD functionality to setlist model

--- setlistsharer/urls.py
+++ setlistsharer/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('home.urls'), name='home_urls'),
+    path('', include('home.urls'), name='home'),
     path('accounts/', include('allauth.urls')),
     path('setlist/', include('setlist.urls')),
 ]
