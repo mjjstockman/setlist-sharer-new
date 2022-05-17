9f6436a2af9f03d9e79cf2b0ed72547a1e5ce027
mjjstockman <mjjstockman@gmail.com>
2022-05-17 05:10:54 +0000

    Add setlist views

--- setlist/views.py
+++ setlist/views.py
@@ -1,16 +1,55 @@
 from django.shortcuts import render, redirect
+from .models import Setlist
 from .forms import SetlistForm
+
+
+
+def all(request):
+    setlists = Setlist.objects.filter(status=1)
+    context = {
+        'setlists': setlists,
+    }
+    return render(request, 'setlist/setlists.html', context)
 
 
 def add(request):
     form = SetlistForm()
-    # if request.method == 'POST':
-    #     form = SetlistForm(request.POST)
-    #     if form.is_valid():
-    #         form.save()
-    #         return redirect('')
+    if request.method == 'POST':
+        form = SetlistForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
 
     context = {
         'form': form
     }
     return render(request, 'add.html', context)
+
+
+def edit(request, pk):
+    setlist = Setlist.objects.get(id=pk)
+    form = SetlistForm(instance=setlist)
+
+    if request.method == 'POST':
+        form = SetlistForm(request.POST, instance=setlist)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'setlist/add.html', context)
+
+
+def delete(request, pk):
+    setlist = Setlist.objects.get(id=pk)
+
+    if request.method == 'POST':
+        setlist.delete()
+        return redirect('/')
+        
+    context = {
+        'setlist': setlist,
+    }
+    return render(request, 'setlist/delete.html', context)
