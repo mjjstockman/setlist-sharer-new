0e8ab1c5cf5d15f4074fbd450009b52640a9f04c
mjjstockman <mjjstockman@gmail.com>
2022-06-01 18:38:38 +0000

    Add gigs_setlist_pending query to gigs view

--- home/views.py
+++ home/views.py
@@ -8,12 +8,14 @@ def gigs(request):
 
     gigs = Gig.objects.all()
 
-    gigs_setlist_published = Setlist.objects.get(status=1)
-    # gigs_setlist_pending = Gig.objects.filter(status=0)
+    gigs_setlist_published = Setlist.objects.filter(status=1)
+    gigs_setlist_pending = Setlist.objects.filter(status=0)
+
+   
    
     context = {
         'published': gigs_setlist_published,
-        # 'pending': gigs_setlist_pending,
+        'pending': gigs_setlist_pending,
         'gigs': gigs,
     }
     return render(request, 'index.html', context)
