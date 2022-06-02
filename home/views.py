59e9b28f6a5c182657384790ed296afd4f66a36c
mjjstockman <mjjstockman@gmail.com>
2022-06-02 15:38:05 +0000

    Return gig setlist status login into home template

--- home/views.py
+++ home/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
-from .models import Gig
 from setlist.models import Setlist
+from .models import Gig
+
 
 # Create your views here.
 
 def gigs(request):
+    gigs = Gig.objects.all().order_by('date')
     gigs_setlist_published = Setlist.objects.filter(status=1)
     gigs_setlist_pending = Setlist.objects.filter(status=0)
     gigs_no_setlist = Gig.objects.filter(setlist_gig__isnull=True)
 
     context = {
+        'gigs': gigs,
         'published': gigs_setlist_published,
         'pending': gigs_setlist_pending,
         'no_list': gigs_no_setlist,
