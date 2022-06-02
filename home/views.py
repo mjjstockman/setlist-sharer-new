9fc64654e3afb6fcdec6fd7651d3a3bb91466045
mjjstockman <mjjstockman@gmail.com>
2022-06-02 08:11:40 +0000

    Add gigs_no_setlist query to gigs view

--- home/views.py
+++ home/views.py
@@ -5,17 +5,13 @@ from setlist.models import Setlist
 # Create your views here.
 
 def gigs(request):
-
-    gigs = Gig.objects.all()
-
     gigs_setlist_published = Setlist.objects.filter(status=1)
     gigs_setlist_pending = Setlist.objects.filter(status=0)
+    gigs_no_setlist = Gig.objects.filter(setlist_gig__isnull=True)
 
-   
-   
     context = {
         'published': gigs_setlist_published,
         'pending': gigs_setlist_pending,
-        'gigs': gigs,
+        'no_list': gigs_no_setlist,
     }
     return render(request, 'index.html', context)
