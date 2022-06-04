84752cf564fb50f7354b44ab0bf27d1d5ba97696
mjjstockman <mjjstockman@gmail.com>
2022-06-04 12:48:26 +0000

    Correct display of gigs in home template

--- home/views.py
+++ home/views.py
@@ -6,30 +6,30 @@ from .models import Gig
 # Create your views here.
 
 def gigs(request):
-    # gigs = Gig.objects.all().order_by('date')
-    gigs_setlist_published = Setlist.objects.filter(status=1)
-    gigs_setlist_pending = Setlist.objects.filter(status=0)
-    gigs_no_setlist = Gig.objects.filter(setlist_gig__isnull=True)
+    gigs = Gig.objects.all().order_by('date')
+    # gigs_setlist_published = Setlist.objects.filter(status=1)
+    # gigs_setlist_pending = Setlist.objects.filter(status=0)
+    # gigs_no_setlist = Gig.objects.filter(setlist_gig__isnull=True)
 
-    gigs_setlist = []
-    for gig in gigs_setlist_published:
-        gigs_setlist.append(gig)
+    # gigs_setlist = []
+    # for gig in gigs_setlist_published:
+    #     gigs_setlist.append(gig)
 
-    for gig in gigs_setlist_pending:
-        gigs_setlist.append(gig)
+    # for gig in gigs_setlist_pending:
+    #     gigs_setlist.append(gig)
 
-    for gig in gigs_no_setlist:
-        gigs_setlist.append(gig)
+    # for gig in gigs_no_setlist:
+    #     gigs_setlist.append(gig)
 
 
 
 
 
     context = {
-        # 'gigs': gigs,
+        'gigs': gigs,
         # 'published': gigs_setlist_published,
         # 'pending': gigs_setlist_pending,
         # 'no_list': gigs_no_setlist,
-        'gigs_setlist': gigs_setlist,
+        # 'gigs_setlist': gigs_setlist,
     }
     return render(request, 'index.html', context)
