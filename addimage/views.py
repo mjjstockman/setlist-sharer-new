93a193faafc2332ed1c684ee7f3db98d0caa7eb1
mjjstockman <mjjstockman@gmail.com>
2022-06-11 22:15:58 +0000

    User can select an image to upload

--- addimage/views.py
+++ addimage/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import AddImageForm
+from cloudinary.forms import cl_init_js_callbacks
+
+
+
 
 # Create your views here.
 def add_image(request):
-    return render(request, 'addimage/add_image.html')
+    form = AddImageForm(request.POST, request.FILES)
+    if request.method == 'POST':
+        # form = AddImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+
+    # form = AddImageForm()
+    context = {'form': form}
+
+    return render(request, 'addimage/add_image.html', context)
